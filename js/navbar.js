ade00269414c1a8bb9a7a2dfa50b9c9e362075aa
vividcurlyfry <amy.pegram@hotmail.com>
2020-04-22 19:06:21 +0200

    fixing nav

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,6 @@
 function generateNav() {
     document.getElementsByTagName("nav")[0].innerHTML +=
-        `   <a href="/WSOA3028A_1825142/index.html">Home</a>
+        `<a href="/WSOA3028A_1825142/index.html">Home</a>
         <a href="/WSOA3028A_1825142/Victims/">The Victims</a>
         <a href="/WSOA3028A_1825142/Blogs/">Blogs</a>
         <a href="/WSOA3028A_1825142/Contact.html">Contact</a>`;
