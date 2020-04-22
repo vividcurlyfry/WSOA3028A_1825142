92a435f100a5b5e897ef4dc1df68b68b124a5a19
vividcurlyfry <amy.pegram@hotmail.com>
2020-04-22 17:51:11 +0200

    working!!!!!!!

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,7 @@
 function generateNav() {
-    document.body.innerHTML += `<nav>
-        <a href="/WSOA3028A_1825142/index.html">Home</a>
+    document.getElementsByTagName("nav")[0].innerHTML +=
+        `   <a href="/WSOA3028A_1825142/index.html">Home</a>
         <a href="/WSOA3028A_1825142/Victims/">The Victims</a>
         <a href="/WSOA3028A_1825142/Blogs/">Blogs</a>
-        <a href="/WSOA3028A_1825142/Contact.html">Contact</a>
-    </nav>`;
+        <a href="/WSOA3028A_1825142/Contact.html">Contact</a>`;
 }
