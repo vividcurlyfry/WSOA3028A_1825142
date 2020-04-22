4952ee156d0fc0a132808ab617d785f722db1743
vividcurlyfry <amy.pegram@hotmail.com>
2020-04-22 17:20:30 +0200

    trying again

--- navbar.js
+++ navbar.js
@@ -1,8 +1,8 @@
 function generateNav() {
-    document.getElementById("navgen").innerHTML += `<nav>
+    document.body.innerHTML += `<nav>
         <a href="/WSOA3028A_1825142/index.html">Home</a>
         <a href="/WSOA3028A_1825142/Victims/">The Victims</a>
         <a href="/WSOA3028A_1825142/Blogs/">Blogs</a>
         <a href="/WSOA3028A_1825142/Contact.html">Contact</a>
-    </nav>`
+    </nav>`;
 }
