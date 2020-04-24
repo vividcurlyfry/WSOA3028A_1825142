9bb0412e5aceb1889e9db599176a99f74023bcb6
vividcurlyfry <amy.pegram@hotmail.com>
2020-04-24 16:16:30 +0200

    Contact changing

--- js/navbar.js
+++ js/navbar.js
@@ -4,15 +4,16 @@ function generateNav() {
         <a href="/WSOA3028A_1825142/index.html">HOME</a>
         <a href="/WSOA3028A_1825142/Victims/">VICTIMS</a>
         <a href="/WSOA3028A_1825142/Blogs/">BLOGS</a>
-        <a href="/WSOA3028A_1825142/Contact.html">CONTACT</a>
         </div>`;
 }
 
 function addFooter() {
     document.getElementsByTagName("footer")[0].innerHTML +=
-        `<p>If you have any information on the crimes, please don't hesitate to contact me:</p>
-        <address>
-            <a>Sadie.Garner@hotmail.com</a><br>
-            <a>011 453 5608</a>
-        </address>`
+        `<div>
+        <p style="padding:5px">If you have any information relating to the the crimes, please don't hesitate to contact me:</p>
+        <ul style="list-style-type: none;">
+            <li>Sadie.Garner@hotmail.com</a>
+            <li style="padding:5px">011 453 5608</a>
+        </ul>
+        </div>`
 }
