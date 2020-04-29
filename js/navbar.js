e9f4af236fd180fa86fc5898b748bb26082c9b65
vividcurlyfry <amy.pegram@hotmail.com>
2020-04-29 16:16:30 +0200

    dropdown working

--- js/navbar.js
+++ js/navbar.js
@@ -1,25 +1,29 @@
 function generateNav() {
     document.getElementsByTagName("nav")[0].innerHTML +=
-        ` <div class="topnav">
-        <button><a href="/WSOA3028A_1825142/index.html">Sadie Garner</a></button>
-        <button><a href="/WSOA3028A_1825142/Victims/">VICTIMS</a></button>
-        <button class="blogdrop" style="border-bottom: 2px solid #F3EDEA;"><a href="#">BLOGS</button>
-    </div>
-    <div class="blogdrop">
-        <ul>
-            <li><a href="/WSOA3028A_1825142/Blogs/Blog1-RRLibraryOfBabel.html">Library of Babel</a>
-            </li>
-            <li><a href="/WSOA3028A_1825142/Blogs/Blog2-Character Description.html">Character Description</a>
-            </li>
-            <li><a href="/WSOA3028A_1825142/Blogs/Blog3-RRComputingMachiney.html">Computing Machinery and
+        `<ul>
+            <a id="home" href="/WSOA3028A_1825142/index.html">Sadie Garner</a>
+            <li><a style= "border-bottom: 2px solid #F3EDEA;" href="#">BLOGS</a>
+                <ul>
+                    <li><a style= "border: none" href="/WSOA3028A_1825142/Blogs/Blog1-RRLibraryOfBabel.html">Library of Babel</a>
+                    </li>
+                    <li><a style= "border: none" href="/WSOA3028A_1825142/Blogs/Blog2-Character Description.html">Character
+                    Description</a>
+                    </li>
+                    <li><a style= "border: none" href="/WSOA3028A_1825142/Blogs/Blog3-RRComputingMachiney.html">Computing Machinery and
                     Intelligence</a></li>
-            <li><a href="/WSOA3028A_1825142/Blogs/Blog4-WhatAreBrowsers.html">What are Browsers and URLs?</a>
+                    <li><a style= "border: none" href="/WSOA3028A_1825142/Blogs/Blog4-WhatAreBrowsers.html">What are Browsers and
+                    URLs?</a>
+                    </li>
+                    <li> <a style= "border: none" href="/WSOA3028A_1825142/Blogs/Blog5-Accessibility.html">The Importance of
+                    Accessibility</a>
+                    </li>
+                    <li><a style= "border: none"
+                        href="/WSOA3028A_1825142/Blogs/Blog6-CreativeBrainstorm.html">Creative Brainstorm</a>
+                    </li>
+                </ul>
             </li>
-            <li> <a href="/WSOA3028A_1825142/Blogs/Blog5-Accessibility.html">The Importance of Accessibility</a>
-            </li>
-            <li style="border-bottom: none;"><a href="/WSOA3028A_1825142/Blogs/Blog6-CreativeBrainstorm.html">Creative Brainstorm</a></li>
-        </ul>
-    </div>`;
+            <li><a href="/WSOA3028A_1825142/Victims/">VICTIMS</a></li>
+        </ul>`
 }
 
 function addFooter() {
