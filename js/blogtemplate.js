77c409f932f6c34ae231500edb102b042a08ac70
vividcurlyfry <amy.pegram@hotmail.com>
2020-06-10 15:44:25 +0200

    adding some meta data stuff

--- js/blogtemplate.js
+++ js/blogtemplate.js
@@ -1,51 +1,56 @@
-function LoadBlogPost(BlogTitle, BlogContent) {
-    document.getElementById("addBlog").innerHTML +=
-        ` <head>
-        <title>Sadie Garner</title>
+function LoadBlogPost(BlogTitle, BlogContent, BlogLocation) {
+    document.querySelector("head").innerHTML +=
+        `<title>Sadie Garner</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta charset="utf-8">
         <meta name="author" content="Amy Pegram, 1825142">
-        <script src="/WSOA3028A_1825142/js/navbar.js" async></script>
-        <link href="/WSOA3028A_1825142/css/stylesheet.css" rel="stylesheet">
-    </head>
-    
-    <body>
+        <meta name="description" content="Blog: `+ BlogTitle + `">
+        <link rel = "icon" href = "/WSOA3028A_1825142/Logo.png" type = "image/png">
+        <meta property="og:title" content="Sadie Garner" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://vividcurlyfry.github.io/WSOA3028A_1825142/`+ BlogLocation + `" />
+        <meta property="og:image" content="https://vividcurlyfry.github.io/WSOA3028A_1825142/Sadie.jpg" />
+        <meta property="og:description" content="Blog: ` + BlogTitle + ` ">
+
+        <link href="/WSOA3028A_1825142/css/stylesheet.css" rel="stylesheet">`;
+
+    document.querySelector("body").innerHTML +=
+        `
         <nav>
-        <ul>
-            <a id="home" href="/WSOA3028A_1825142/index.html"><img src="/WSOA3028A_1825142/Logo.png" alt="Logo: A picture of the scales of justice." height=30rem> Sadie Garner</a>
-            <li><a href="/WSOA3028A_1825142/contact.html">CONTACT</a></li>
-            <li><a style= "border-bottom: 0.12rem solid #F3EDEA;" href="#">BLOGS</a>
-                <ul>
-                    <li><a style= "border: none" href="/WSOA3028A_1825142/Blogs/Blog1-RRLibraryOfBabel.html">Library of Babel</a>
-                    </li>
-                    <li><a style= "border: none" href="/WSOA3028A_1825142/Blogs/Blog2-Character Description.html">Character
-                    Description</a>
-                    </li>
-                    <li><a style= "border: none" href="/WSOA3028A_1825142/Blogs/Blog3-RRComputingMachiney.html">Computing Machinery and
-                    Intelligence</a></li>
-                    <li><a style= "border: none" href="/WSOA3028A_1825142/Blogs/Blog4-WhatAreBrowsers.html">What are Browsers and
-                    URLs?</a>
-                    </li>
-                    <li> <a style= "border: none" href="/WSOA3028A_1825142/Blogs/Blog5-Accessibility.html">The Importance of
-                    Accessibility</a>
-                    </li>
-                    <li><a style= "border: none"
-                        href="/WSOA3028A_1825142/Blogs/Blog6-CreativeBrainstorm.html">Creative Brainstorm</a>
-                    </li>
-                    <li><a style= "border: none"
-                        href="/WSOA3028A_1825142/Blogs/Blog7-ActuallyFiguringItOut.html">Actually Figuring It Out</a>
-                    </li>
-                    <li><a style= "border: none"
-                        href="/WSOA3028A_1825142/Blogs/Blog9-BeingIntoTrueCrime.html">Being “Into” True Crime But Also Believing All Cops Are Bastards</a>
-                    </li>
-                </ul>
-            </li>
-            <li><a href="/WSOA3028A_1825142/Victims/">VICTIMS</a></li>
-        </ul>
-        </nav>   
+            <ul>
+                <a id="home" href="/WSOA3028A_1825142/index.html"><img src="/WSOA3028A_1825142/Logo.png" alt="Logo: A picture of the scales of justice." height=30rem> Sadie Garner</a>
+                <li><a href="/WSOA3028A_1825142/contact.html">CONTACT</a></li>
+                <li><a style="border-bottom: 0.12rem solid #F3EDEA;" href="#">BLOGS</a>
+                    <ul>
+                        <li><a style="border: none" href="/WSOA3028A_1825142/Blogs/Blog1-RRLibraryOfBabel.html">Library of Babel</a>
+                        </li>
+                        <li><a style="border: none" href="/WSOA3028A_1825142/Blogs/Blog2-Character Description.html">Character
+        Description</a>
+                        </li>
+                        <li><a style="border: none" href="/WSOA3028A_1825142/Blogs/Blog3-RRComputingMachiney.html">Computing Machinery and
+        Intelligence</a></li>
+                        <li><a style="border: none" href="/WSOA3028A_1825142/Blogs/Blog4-WhatAreBrowsers.html">What are Browsers and
+        URLs?</a>
+                        </li>
+                        <li> <a style="border: none" href="/WSOA3028A_1825142/Blogs/Blog5-Accessibility.html">The Importance of
+        Accessibility</a>
+                        </li>
+                        <li><a style="border: none"
+                            href="/WSOA3028A_1825142/Blogs/Blog6-CreativeBrainstorm.html">Creative Brainstorm</a>
+                        </li>
+                        <li><a style="border: none"
+                            href="/WSOA3028A_1825142/Blogs/Blog7-ActuallyFiguringItOut.html">Actually Figuring It Out</a>
+                        </li>
+                        <li><a style="border: none"
+                            href="/WSOA3028A_1825142/Blogs/Blog9-BeingIntoTrueCrime.html">Being “Into” True Crime But Also Believing All Cops Are Bastards</a>
+                        </li>
+                    </ul>
+                </li>
+                <li><a href="/WSOA3028A_1825142/Victims/">VICTIMS</a></li>
+            </ul>
+        </nav>
         <section class="container">
-            <h1 class = "headingRow">` + BlogTitle + `</h1>
+            <h1 class="headingRow">` + BlogTitle + `</h1>
             <article class="articleRow"> ` + BlogContent + `</article>
-        </section>
-    </body>`;
+        </section>`;
 }
--- js/slideshow.js
+++ js/slideshow.js
@@ -1,34 +1,42 @@
 const image1 = new Image()
 image1.src = "/WSOA3028A_1825142/Victims/VictimsPics/Victim1.png"
 image1.height = "200rem"
+image1.alt = "Nhlanhla Visser, Age 13"
 
 const image2 = new Image()
 image2.src = "/WSOA3028A_1825142/Victims/VictimsPics/Victim2.png"
 image2.height = "200rem"
+image2.alt = "Vincent Khoza, Age 8"
 
 const image3 = new Image()
 image3.src = "/WSOA3028A_1825142/Victims/VictimsPics/Victim3.png"
 image3.height = "200rem"
+image3.alt = "Terence Motaung, Age 14"
 
 const image4 = new Image()
 image4.src = "/WSOA3028A_1825142/Victims/VictimsPics/Victim4.png"
 image4.height = "200rem"
+image4.alt = "Kgomotso Ndhlovu, Age 15"
 
 const image5 = new Image()
 image5.src = "/WSOA3028A_1825142/Victims/VictimsPics/Victim5.png"
 image5.height = "200rem"
+image5.alt = "Pamela Njandi, Age 13"
 
 const image6 = new Image()
 image6.src = "/WSOA3028A_1825142/Victims/VictimsPics/Victim6.png"
 image6.height = "200rem"
+image6.alt = "Nelly Khumalo, Age 14"
 
 const image7 = new Image()
 image7.src = "/WSOA3028A_1825142/Victims/VictimsPics/Victim7.png"
 image7.height = "200rem"
+image7.alt = "Pinimidzai Maseko, Age 15"
 
 const image8 = new Image()
 image8.src = "/WSOA3028A_1825142/Victims/VictimsPics/Victim8.png"
 image8.height = "200rem"
+image8.alt = "Sheronisa Dladla, Age 8"
 
 let step = 1
 function slideshow() {
