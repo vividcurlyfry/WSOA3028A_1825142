287221c90239a93263346d37fea880548b7e403e
vividcurlyfry <amy.pegram@hotmail.com>
2020-06-11 23:32:10 +0200

    adding blogs

--- js/blogtemplate.js
+++ js/blogtemplate.js
@@ -15,41 +15,8 @@ function LoadBlogPost(BlogTitle, BlogContent, References, BlogLocation) {
         <meta property="og:image" content="WSOA3028A_1825142/Logo.png" />
         <meta property="og:description" content="Blog: ` + BlogTitle + ` ">
 
-        <link href="/WSOA3028A_1825142/css/stylesheet.css" rel="stylesheet">`;
-    document.querySelector("body").innerHTML +=
-        `<nav>
-            <ul>
-                <a id="home" href="/WSOA3028A_1825142/index.html"><img src="/WSOA3028A_1825142/Logo.png" alt="Logo: A picture of the scales of justice." height=30rem> Sadie Garner</a>
-                <li><a href="/WSOA3028A_1825142/contact.html">CONTACT</a></li>
-                <li><a style="border-bottom: 0.12rem solid #F3EDEA;" href="#">BLOGS</a>
-                    <ul>
-                        <li><a style="border: none" href="/WSOA3028A_1825142/Blogs/Blog1-RRLibraryOfBabel.html">Library of Babel</a>
-                        </li>
-                        <li><a style="border: none" href="/WSOA3028A_1825142/Blogs/Blog2-Character Description.html">Character
-            Description</a>
-                        </li>
-                        <li><a style="border: none" href="/WSOA3028A_1825142/Blogs/Blog3-RRComputingMachiney.html">Computing Machinery and
-            Intelligence</a></li>
-                        <li><a style="border: none" href="/WSOA3028A_1825142/Blogs/Blog4-WhatAreBrowsers.html">What are Browsers and
-            URLs?</a>
-                        </li>
-                        <li> <a style="border: none" href="/WSOA3028A_1825142/Blogs/Blog5-Accessibility.html">The Importance of
-            Accessibility</a>
-                        </li>
-                        <li><a style="border: none"
-                            href="/WSOA3028A_1825142/Blogs/Blog6-CreativeBrainstorm.html">Creative Brainstorm</a>
-                        </li>
-                        <li><a style="border: none"
-                            href="/WSOA3028A_1825142/Blogs/Blog7-ActuallyFiguringItOut.html">Actually Figuring It Out</a>
-                        </li>
-                        <li><a style="border: none"
-                            href="/WSOA3028A_1825142/Blogs/Blog9-BeingIntoTrueCrime.html">Being “Into” True Crime But Also Believing All Cops Are Bastards</a>
-                        </li>
-                    </ul>
-                </li>
-                <li><a href="/WSOA3028A_1825142/Victims/">VICTIMS</a></li>
-            </ul>
-        </nav>`
+        <link href="/WSOA3028A_1825142/css/stylesheet.css" rel="stylesheet">
+        `;
     if (References != '') {
         document.querySelector("body").innerHTML +=
             `<section class="container">
--- js/navbar.js
+++ js/navbar.js
@@ -1,5 +1,5 @@
 function generateNav() {
-    document.getElementsByTagName("nav")[0].innerHTML +=
+    document.querySelector("nav").innerHTML +=
         `<ul>
             <a id="home" href="/WSOA3028A_1825142/index.html"><img src="/WSOA3028A_1825142/Logo.png" alt="Logo: A picture of the scales of justice." height=30rem> Sadie Garner</a>
             <li><a href="/WSOA3028A_1825142/contact.html">CONTACT</a></li>
@@ -22,10 +22,22 @@ function generateNav() {
                         href="/WSOA3028A_1825142/Blogs/Blog6-CreativeBrainstorm.html">Creative Brainstorm</a>
                     </li>
                     <li><a style= "border: none"
-                        href="/WSOA3028A_1825142/Blogs/Blog7-ActuallyFiguringItOut.html">Actually Figuring It Out</a>
+                        href="/WSOA3028A_1825142/Blogs/Blog7-Javascript.html">Var, Let and Const</a>
                     </li>
                     <li><a style= "border: none"
-                        href="/WSOA3028A_1825142/Blogs/Blog9-BeingIntoTrueCrime.html">Being “Into” True Crime But Also Believing All Cops Are Bastards</a>
+                        href="/WSOA3028A_1825142/Blogs/Blog8-ActuallyFiguringItOut.html">Actually Figuring It Out</a>
+                    </li>
+                    <li><a style= "border: none"
+                        href="/WSOA3028A_1825142/Blogs/Blog9-Tweet.html">Analysing A Tweet</a>
+                    </li>
+                    <li><a style= "border: none"
+                        href="/WSOA3028A_1825142/Blogs/Blog10-BeingIntoTrueCrime.html">Being “Into” True Crime But Also Believing All Cops Are Bastards</a>
+                    </li>
+                    <li><a style= "border: none"
+                        href="/WSOA3028A_1825142/Blogs/Blog11-WireframesAndColour.html">Wireframes and Colour Scheme</a>
+                    </li>
+                    <li><a style= "border: none"
+                        href="/WSOA3028A_1825142/Blogs/Blog12-RRTowardsAStudyOfInfo.html">Close Reading - Towards a study of information geographies: (im)mutable augmentations and a mapping of the geographies of information</a>
                     </li>
                 </ul>
             </li>
