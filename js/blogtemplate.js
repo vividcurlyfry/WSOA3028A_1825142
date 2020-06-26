d0a4ba0f549f19a8af08e9f21e3b9be258a4192c
vividcurlyfry <amy.pegram@hotmail.com>
2020-06-26 15:36:30 +0200

    blog template change

--- js/blogtemplate.js
+++ js/blogtemplate.js
@@ -1,22 +1,4 @@
-function LoadBlogPost(BlogTitle, BlogContent, References, BlogLocation) {
-    document.querySelector("head").innerHTML +=
-        `
-        <title>Sadie Garner</title>
-
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta charset="utf-8">
-        <meta name="author" content="Amy Pegram, 1825142">
-        <meta name="description" content="Blog: `+ BlogTitle + `">
-        <link rel="icon" href="/WSOA3028A_1825142/Logo.png" type="image/png">
-
-        <meta property="og:title" content="Sadie Garner" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://vividcurlyfry.github.io/WSOA3028A_1825142/`+ BlogLocation + `" />
-        <meta property="og:image" content="WSOA3028A_1825142/Logo.png" />
-        <meta property="og:description" content="Blog: ` + BlogTitle + ` ">
-
-        <link href="/WSOA3028A_1825142/css/stylesheet.css" rel="stylesheet">
-        `;
+function LoadBlogPost(BlogTitle, BlogContent, References) {
     if (References != '') {
         document.querySelector("body").innerHTML +=
             `<section class="container">
