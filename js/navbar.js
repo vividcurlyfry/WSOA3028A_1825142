c707c5542aa165f49b231d1877a43dccb5ac1c06
vividcurlyfry <amy.pegram@hotmail.com>
2020-06-29 01:27:56 +0200

    updated blog page!!

--- js/navbar.js
+++ js/navbar.js
@@ -3,12 +3,28 @@ function generateNav() {
         `<ul>
             <a id="home" href="/WSOA3028A_1825142/index.html"><img src="/WSOA3028A_1825142/Logo.png" alt="Logo: A picture of the scales of justice." height=30rem> Sadie Garner</a>
             <li><a href="/WSOA3028A_1825142/contact.html">CONTACT</a></li>
+            <li><a style= "border-bottom: 0.12rem solid #F3EDEA;" href="#">CREATIVE</a>
+                <ul id = "Creative">
+                    <li id = "CreativeList"><a style= "border: none" href="/WSOA3028A_1825142/Blogs/Blog2-CharacterDescription.html">Character
+                    Description</a>
+                    </li>
+                    <li id = "CreativeList"><a style= "border: none"
+                        href="/WSOA3028A_1825142/Blogs/Blog6-CreativeBrainstorm.html">Creative Brainstorm</a>
+                    </li>
+                    <li id = "CreativeList"><a style= "border: none"
+                        href="/WSOA3028A_1825142/Blogs/Blog8-ActuallyFiguringItOut.html">Actually Figuring It Out</a>
+                    </li>
+                    <li id = "CreativeList"><a style= "border: none"
+                        href="/WSOA3028A_1825142/Blogs/Blog10-BeingIntoTrueCrime.html">Being “Into” True Crime But Also Believing All Cops Are Bastards</a>
+                    </li>
+                    <li id = "CreativeList"><a style= "border: none"
+                        href="/WSOA3028A_1825142/Blogs/Blog11-WireframesAndColour.html">Wireframes and Colour Scheme</a>
+                    </li>
+                </ul>
+            </li>
             <li><a style= "border-bottom: 0.12rem solid #F3EDEA;" href="#">BLOGS</a>
                 <ul>
                     <li><a style= "border: none" href="/WSOA3028A_1825142/Blogs/Blog1-RRLibraryOfBabel.html">Library of Babel</a>
-                    </li>
-                    <li><a style= "border: none" href="/WSOA3028A_1825142/Blogs/Blog2-CharacterDescription.html">Character
-                    Description</a>
                     </li>
                     <li><a style= "border: none" href="/WSOA3028A_1825142/Blogs/Blog3-RRComputingMachiney.html">Computing Machinery and
                     Intelligence</a></li>
@@ -19,13 +35,7 @@ function generateNav() {
                     Accessibility</a>
                     </li>
                     <li><a style= "border: none"
-                        href="/WSOA3028A_1825142/Blogs/Blog6-CreativeBrainstorm.html">Creative Brainstorm</a>
-                    </li>
-                    <li><a style= "border: none"
                         href="/WSOA3028A_1825142/Blogs/Blog7-Javascript.html">Var, Let and Const</a>
-                    </li>
-                    <li><a style= "border: none"
-                        href="/WSOA3028A_1825142/Blogs/Blog8-ActuallyFiguringItOut.html">Actually Figuring It Out</a>
                     </li>
                     <li><a style= "border: none"
                         href="/WSOA3028A_1825142/Blogs/Blog9-Tweet.html">Analysing A Tweet</a>
@@ -34,18 +44,15 @@ function generateNav() {
                         href="/WSOA3028A_1825142/Blogs/Blog10-BeingIntoTrueCrime.html">Being “Into” True Crime But Also Believing All Cops Are Bastards</a>
                     </li>
                     <li><a style= "border: none"
-                        href="/WSOA3028A_1825142/Blogs/Blog11-WireframesAndColour.html">Wireframes and Colour Scheme</a>
-                    </li>
-                    <li><a style= "border: none"
                         href="/WSOA3028A_1825142/Blogs/Blog12-RRTowardsAStudyOfInfo.html">Close Reading - Towards a study of information geographies: (im)mutable augmentations and a mapping of the geographies of information</a>
                     </li>
                 </ul>
             </li>
-            <li><a style= "border-bottom: 0.12rem solid #F3EDEA;" href="#">VICTIMS</a>
-            <ul>
-                <li><a style= "border: none" href="/WSOA3028A_1825142/Victims/">Victim's Page</a>
+            <li id = "Victims"><a style= "border-bottom: 0.12rem solid #F3EDEA;" href="#">VICTIMS</a>
+            <ul id = "Victims">
+                <li id = "VictimsList"><a style= "border: none" href="/WSOA3028A_1825142/Victims/">Victims</a>
                 </li>
-                <li><a style= "border: none" href="/WSOA3028A_1825142/Victims/UpdateBlogButton.html">Fake Blog Update</a>
+                <li id = "VictimsList"><a style= "border: none" href="/WSOA3028A_1825142/Victims/UpdateBlogButton.html">Fake Blog Update</a>
             </ul>
         </ul>`
 }
--- js/sendemail.js
+++ js/sendemail.js
@@ -2,11 +2,16 @@
 function sendEmail() {
     Email.send({
         SecureToken: "1ea298c0-7760-4298-9270-42ef2a08fbad",
-        To: 'amy.pegram@hotmail.com',
-        From: "Sadie.Garner@gmail.com",
+        To: document.getElementById("email").value,
+        From: "Sadie.Garner@hotmail.com",
         Subject: "Update on the Township Killer Case!",
-        Body: "There has been an update on the township killer case!"
+        Body: "There has been an update on the township killer case! One Killer. There are a few reasons I have theorised that these killings are all the result of one person. There is a distinct pattern occurring within these killings.I have created a timeline to show the order of the killings"
     }).then(
-        message => alert(message)
+        openPage()
     );
+}
+
+function openPage() {
+    alert("An email has been sent. Please check your junk/spam folder too <3");
+    window.location = "/WSOA3028A_1825142/Victims/UpdatedBlog.html";
 }
